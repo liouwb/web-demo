fadad40cfde1e090c8090c5ae16f1b48f53e49f8
liouwb <liubin@cdiandian.com>
2020-04-29 10:11:02 +0800

    init project

---
+++ README.md
@@ -0,0 +1,8 @@
+# srping boot项目
+
+#### 2020-04-29 10:10
+- 初始化项目
+- 引入lombok
+- mysql jdbc driver
+- 未做任何配置调整
+
---
+++ src/test/java/com/liouxb/web/demo/WebDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liouxb.web.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
