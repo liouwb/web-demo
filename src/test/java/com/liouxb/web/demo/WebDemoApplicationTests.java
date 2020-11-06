ec1ee21e1f8d55f58ead3106aab292fb3585aa64
liouwb <liouxb@163.com>
2020-11-06 08:59:49 +0800

    add mybatis plus test

--- src/test/java/com/liouxb/web/demo/WebDemoApplicationTests.java
+++ src/test/java/com/liouxb/web/demo/WebDemoApplicationTests.java
@@ -1,13 +1,27 @@
 package com.liouxb.web.demo;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.liouxb.web.demo.mapper.TestMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class WebDemoApplicationTests {
+    @Autowired
+    private TestMapper testMapper;
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    public void testSelect() {
+        QueryWrapper<com.liouxb.web.demo.domain.Test> queryWrapper = new QueryWrapper<>();
+        List<com.liouxb.web.demo.domain.Test> userList = testMapper.selectList(queryWrapper);
+
+        userList.forEach(System.out::println);
+    }
 }
